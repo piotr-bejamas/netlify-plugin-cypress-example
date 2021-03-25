fee5edadd1357a8fecca221574e5cd37610cbf90
Piotr Zając <piotr@bejamas.io>
2021-03-25 09:57:02 +0100

    bug added

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -10,7 +10,7 @@ describe('Gatsby blog', () => {
 
   it('navigates to second page and back', () => {
     cy.contains('a', 'Go to page 2').click()
-    cy.url().should('match', /page-2/)
+    cy.url().should('match', /page-3/)
     cy.contains('main > h1', 'Hi from the second page')
     cy.contains('a', 'Go back to the homepage').click()
     cy.url().should('include', Cypress.config('baseUrl'))
